f4c4a4aaab9dd7d5d6ac95a64d2aa3218bf47d3a
sebe324 <sebeq05@gmail.com>
2022-09-23 10:55:28 +0200

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    private var counter: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var message = Toast.makeText(applicationContext, "Hello!!!", Toast.LENGTH_SHORT)
+
+        binding.incrementBtn.setOnClickListener {
+            binding.counter.text = (++counter).toString()
+            message.show()
+        }
+        binding.decrementButton.setOnClickListener {
+            binding.counter.text = (--counter).toString()
+        }
+        binding.counterStep.addTextChangedListener {
+            Toast.makeText(applicationContext, "ChangeListener", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
